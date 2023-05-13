c92a246eb56f8f97d6c7379a333e78ab4bd6cfbd
Nicholas <nickndolo95@gmail.com>
2023-05-13 21:28:42 +0300

    created HomePage component

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,30 +1,30 @@
 
-// import Categories from '../Categories/Categories';
-// import Hero from '../Hero/Hero';
-// import Listings from '../Listings/Listings';
-// import Navbar from '../Navbar/Navbar';
-// import ContactUs from '../ContactUs/ContactUs';
-// import Testimonials from '../Testimonials/Testimonials';
-// import Footer from '../Footer/Footer';
-// import './HomePage.css'
+import Categories from '../Categories/Categories';
+import Hero from '../Hero/Hero';
+import Listings from '../Listings/Listings';
+import Navbar from '../Navbar/Navbar';
+import ContactUs from '../ContactUs/ContactUs';
+import Testimonials from '../Testimonials/Testimonials';
+import Footer from '../Footer/Footer';
+import './HomePage.css'
 
 
-// function HomePage({vehicleList}) {
+function HomePage({vehicleList}) {
 
   
   
-//   return (
-//     <div className="home">
-//       <Navbar/>
-//       <Hero/>
-//       <Categories vehicleList={vehicleList}/>
-//       <Listings vehicleList={vehicleList}/>
-//       <ContactUs/>
-//       <Testimonials vehicleList={vehicleList}/>
-//       <Footer />  
+  return (
+    <div className="home">
+      <Navbar/>
+      <Hero/>
+      <Categories vehicleList={vehicleList}/>
+      <Listings vehicleList={vehicleList}/>
+      <ContactUs/>
+      <Testimonials vehicleList={vehicleList}/>
+      <Footer />  
        
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
 
-// export default HomePage;
+export default HomePage;
