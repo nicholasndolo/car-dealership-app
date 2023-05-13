eb606f14a7f1f2d89a0542782715f783019c3311
Nicholas <nickndolo95@gmail.com>
2023-05-13 20:04:20 +0300

    created AdminDashboard component

---
+++ src/components/NavarBar.js
@@ -0,0 +1,30 @@
+// import React, {useRef} from 'react';
+// //import { Link } from 'react-router-dom'; 
+// import { FaBars, FaTimes } from 'react-icons/fa';
+// import "../styles/Navbar.css";
+
+// function NavBar() {
+//     const navRef = useRef()
+
+//     const showNavbar = () => {
+//         navRef.current.classList.toggle("responsive-nav")
+//     }
+//     return (
+//         <header>
+//             <h3>Logo</h3>
+//           <nav ref= {navRef}>
+//               <a href="/#">Home</a>
+//               <a href="/#">About Us</a>
+//               <a href="/#">Contact us</a>
+//               <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+//                   <FaTimes />
+//               </button>
+//           </nav>
+//             <button className="nav-btn" onClick={{showNavbar}}>
+//                 <FaBars />
+//             </button>
+//         </header>
+//     )
+// }
+
+// export default NavBar
