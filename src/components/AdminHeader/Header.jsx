e7820768d85784b7213fa27b2a30f55ba4267866
Nicholas <nickndolo95@gmail.com>
2023-05-13 21:20:19 +0300

    created AdminHeadercomponent

--- src/components/AdminHeader/Header.jsx
+++ src/components/AdminHeader/Header.jsx
@@ -1,22 +1,22 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-// import image1 from "../../assets/image1.jpg";
-// import "./Header.css"
+import React from 'react'
+import { Link } from 'react-router-dom'
+import image1 from "../../assets/image1.jpg";
+import "./Header.css"
 
-// const Header = () => {
-//   return (
-//     <header className="head">
-//         <div className="header-l">
-//           <span>Car Inventory Dashboard</span>
-//         </div>
-//         <div className="header-r">
-//          <img src={image1} alt="" />
-//          <span>Nicholas Ndolo</span>
-//          <Link to='/' className="home-btn">Go back</Link>
+const Header = () => {
+  return (
+    <header className="head">
+        <div className="header-l">
+          <span>Car Inventory Dashboard</span>
+        </div>
+        <div className="header-r">
+         <img src={image1} alt="" />
+         <span>Nicholas Ndolo</span>
+         <Link to='/' className="home-btn">Go back</Link>
          
-//         </div>
-//     </header>
-//   )
-// }
+        </div>
+    </header>
+  )
+}
 
-// export default Header
+export default Header
