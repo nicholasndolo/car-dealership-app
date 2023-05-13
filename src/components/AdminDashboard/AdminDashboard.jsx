d450f13bd9a7627f33ae559b19ba19e33acfbf2a
Nicholas <nickndolo95@gmail.com>
2023-05-13 20:52:31 +0300

    added readmefile

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,19 +1,19 @@
-// import React from 'react'
-// // import logo from "../../assets/logo.png"
-// // import {MdSearch} from "react-icons/md";
-// // import "./AdminDashboard.css"
-//  import Header from '../AdminHeader/Header';
-//  import ListingTable from '../ListingTable/ListingTable';
-//  import Search from '../Search/Search';
+import React from 'react'
+// import logo from "../../assets/logo.png"
+// import {MdSearch} from "react-icons/md";
+// import "./AdminDashboard.css"
+ import Header from '../AdminHeader/Header';
+ import ListingTable from '../ListingTable/ListingTable';
+ import Search from '../Search/Search';
 
-// const AdminDashboard = ({vehicleList,  setVehicleList}) => {
-//   return (
-//     <div className="test">
-//        <Header />
-//       <Search/> 
-//        <ListingTable vehicleList={vehicleList}  setVehicleList={setVehicleList}/>
-//     </div>
-//   )
-// }
+const AdminDashboard = ({vehicleList,  setVehicleList}) => {
+  return (
+    <div className="test">
+       <Header />
+      <Search/> 
+       <ListingTable vehicleList={vehicleList}  setVehicleList={setVehicleList}/>
+    </div>
+  )
+}
 
-// export default AdminDashboard;
+export default AdminDashboard;
